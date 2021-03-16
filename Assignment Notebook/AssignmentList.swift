760393cfbd126e2459cb41b65230bb0c7811cc26
Johnathan Bergman <jbergman@bsd220.org>
2021-03-15 20:58:49 -0500

    Created list of things

---
+++ Assignment Notebook/AssignmentList.swift
@@ -0,0 +1,8 @@
+//
+//  AssignmentList.swift
+//  Assignment Notebook
+//
+//  Created by Student on 3/15/21.
+//
+
+import Foundation
---
+++ Assignment Notebook/SwiftUI.swift
@@ -0,0 +1,20 @@
+//
+//  SwiftUI.swift
+//  Assignment Notebook
+//
+//  Created by Student on 3/15/21.
+//
+
+import SwiftUI
+
+struct SwiftUI: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct SwiftUI_Previews: PreviewProvider {
+    static var previews: some View {
+        SwiftUI()
+    }
+}
