58465a16d4a3f8a9864fc6f652ec57dc526125ce
Johnathan Bergman <jbergman@bsd220.org>
2021-03-18 11:47:50 -0500

    Finished assignment List

--- Assignment Notebook/ContentView.swift
+++ Assignment Notebook/ContentView.swift
@@ -8,14 +8,44 @@
 import SwiftUI
 
 struct ContentView: View {
+    @ObservedObject var assignmentList = AssignmentList()
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        NavigationView {
+            List  {
+                ForEach(toDoList.items) {item in
+                    HStack {
+                        VStack(alignment: .leading) {
+                            Text(item.priority)
+                                .font(.headline)
+                            Text(item.description)
+                        }
+                        Spacer()
+                        Text(item.dueDate, style: .date)
+                    }
+                }
+                .onMove(perform: { indices, newOffset in
+                    toDoList.items.move(fromOffsets: indices, toOffset: newOffset)
+                })
+                .onDelete(perform: { indexSet in
+                    toDoList.items.remove(atOffsets: indexSet)
+                })
+            }
+            .navigationBarTitle("To Do List")
+            .navigationBarItems(leading: EditButton())
+        }
     }
-}
-
-struct ContentView_Previews: PreviewProvider {
-    static var previews: some View {
-        ContentView()
+    
+    
+    struct ContentView_Previews: PreviewProvider {
+        static var previews: some View {
+            ContentView()
+        }
+    }
+    
+    struct ToDoItem: Identifiable {
+        var id = UUID()
+        var priority = String()
+        var description = String()
+        var dueDate = Date()
     }
 }
