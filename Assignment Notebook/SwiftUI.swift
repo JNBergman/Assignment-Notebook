af4535500bf851f985ddc005a08b912c7db12b73
Johnathan Bergman <jbergman@bsd220.org>
2021-05-21 10:29:05 -0500

    Finished Project

--- Assignment Notebook/SwiftUI.swift
+++ Assignment Notebook/SwiftUI.swift
@@ -7,14 +7,43 @@
 
 import SwiftUI
 
-struct SwiftUI: View {
+struct AddView: View {
+    @ObservedObject var assignmentList: AssignmentList
+    @State private var course = ""
+    @State private var description = ""
+    @State private var dueDate = Date()
+    @Environment(\.presentationMode) var presentationMode
+    static let courses = ["English", "Math", "Computer Science", "Biology", "US History"]
+    
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        NavigationView {
+            Form {
+                Picker("Course", selection: $course) {
+                    ForEach(Self.courses, id: \.self) { course in
+                        Text(course)
+                    }
+                }
+                TextField("Description", text: $description)
+                DatePicker("Due Date", selection: $dueDate,
+                           displayedComponents: .date)
+            }
+            .navigationBarTitle("Add New Assignment")
+            .navigationBarItems(trailing: Button("Save") {
+                if course.count > 0 && description.count > 0 {
+                    let item = Assignment(id: UUID(), course: course,
+                                          description: description, dueDate: dueDate)
+                    assignmentList.items.append(item)
+                    presentationMode.wrappedValue.dismiss()
+                }
+            })
+        }
     }
 }
 
-struct SwiftUI_Previews: PreviewProvider {
+struct AddView_Previews: PreviewProvider {
     static var previews: some View {
-        SwiftUI()
+        AddView(assignmentList: AssignmentList())
     }
 }
+
+
